50ed4aabe7cbaa2d65bb51178f003340cc3565ab
Quokka <sm.kim@last30min.com>
2022-07-26 16:26:19 +0900

    eslint 충돌 수정

--- blog/.eslintrc.js
+++ blog/.eslintrc.js
@@ -1,30 +1,33 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es2021: true,
-  },
-  extends: [
-    'airbnb',
-    'plugin:prettier/recommended',
-    // 다른 config를 사용하더라도 prettier를 맨 마지막에 넣어야 모든 중복 규칙을 비활성화 시킬 수 있다.
-    'prettier/react',
-    'eslint:recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    "root": true,
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    ecmaVersion: 'latest',
-  },
-  plugins: ['react', '@typescript-eslint'],
-  rules: {
-    'react/jsx-filename-extension': [
-      'error',
-      {
-        extensions: ['.js', '.jsx', '.tsx'],
-      },
+    "extends": [
+        "eslint:recommended",
+        'plugin:@next/next/recommended',
+        "plugin:@typescript-eslint/recommended"
     ],
-  },
-};
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+        // "@typescript-eslint/strict-boolean-expressions": [
+        //     2,
+        //     {
+        //         "allowString" : false,
+        //         "allowNumber" : false
+        //     }
+        // ]
+    }
+}

08bfc1e222eda9e1dea209d44dde6308ce2d77b2
Quokka <sm.kim@last30min.com>
2022-07-26 11:28:40 +0900

    tailwind css 적용 테스트

--- blog/pages/_app.tsx
+++ blog/pages/_app.tsx
@@ -1,7 +1,7 @@
-import "../styles/globals.css";
-import "antd/dist/antd.css";
-import type { AppProps } from "next/app";
-import Layout from "../src/layout";
+import '../styles/globals.css';
+import 'antd/dist/antd.css';
+import type { AppProps } from 'next/app';
+import Layout from '../src/layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (

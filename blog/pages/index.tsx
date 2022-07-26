d309443597b0aa21c316cf967e2bcac417e03a1d
Quokka <sm.kim@last30min.com>
2022-07-26 11:17:00 +0900

    충돌 수정

--- blog/pages/index.tsx
+++ blog/pages/index.tsx
@@ -1,7 +1,6 @@
 export default function Home() {
   return (
     <>
-      <title>sunmin's blog</title>
       <div>
         <div className="text-3xl">테스트 입니다.</div>
         <div className="text-2xl">테스트입니다.</div>

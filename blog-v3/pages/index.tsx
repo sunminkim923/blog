5ebac44a196a1a5cdcc5043b30f322306a8b61e2
Quokka <sm.kim@last30min.com>
2022-07-30 15:45:17 +0900

    프로필url추가

--- blog-v3/pages/index.tsx
+++ blog-v3/pages/index.tsx
@@ -13,14 +13,64 @@ export default function Home() {
       </Head>
       <div className="flex justify-center  bg-slate-600 ">
         <div className="w-full md:w-[800px] bg-orange-300 ">
-          <div className="text-center text-3xl mt-3 font-bold">
-            안녕하세요. 김선민입니다.
-          </div>
+          <div className="text-center text-3xl mt-3 font-bold">hello</div>
           <div className="flex justify-center">
-            <img
+            {/* <img
               src="/images/profile.jpg"
               className="w-40 mt-6  rounded-full"
-            />
+            /> */}
+            <span className="w-40 h-40 mt-6 bg-slate-400 rounded-full"></span>
+          </div>
+
+          <div className="pl-10">
+            <div className="text-2xl pt-10">About Me.</div>
+            <div className="text-lg pt-2">
+              <li>
+                Github :
+                <a
+                  className="pl-2"
+                  href="https://github.com/sunminkim923"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  https://github.com/sunminkim923
+                </a>
+              </li>
+              <li>
+                Tech Blog :
+                <a
+                  className="pl-2"
+                  // href="https://github.com/sunminkim923"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  https://github.com/sunminkim923
+                </a>
+              </li>
+              <li>
+                Email :
+                <a
+                  className="pl-2"
+                  href="mailto:sunmin.dev94@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  sunmin.dev94@gmail.com
+                </a>
+              </li>
+
+              <li>
+                LinkedIn :
+                <a
+                  className="pl-2"
+                  href="https://www.linkedin.com/in/sunmindev94/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  https://www.linkedin.com/in/sunmindev94/
+                </a>
+              </li>
+            </div>
           </div>
         </div>
       </div>

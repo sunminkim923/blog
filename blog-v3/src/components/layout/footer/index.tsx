4896ce42cb840afa768b93f721dc3b34bffe95ba
sunminkim923 <tjsals0406@gmail.com>
2022-08-03 23:31:30 +0900

    footer 작업중

--- blog-v3/src/components/layout/footer/index.tsx
+++ blog-v3/src/components/layout/footer/index.tsx
@@ -1,7 +1,19 @@
+import { BsGithub } from 'react-icons/bs';
+
 export default function Footer() {
   return (
     <>
-      <div className="text-4xl">footer</div>
+      <div className="h-32 bg-black">
+        <div className="flex items-center h-[100%] justify-center">
+          <a
+            href="https://github.com/sunminkim923"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <BsGithub className="text-[40px] text-white cursor-pointer" />
+          </a>
+        </div>
+      </div>
     </>
   );
 }
